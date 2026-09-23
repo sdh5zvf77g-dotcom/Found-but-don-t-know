5617be054b0a8e0ebd0c32cdf01d6a3eea222126
sdh5zvf77g-dotcom <sdh5zvf77g@privaterelay.appleid.com>
2026-09-23 22:35:10 +0700

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,10 @@
+const CACHE="grok-build-mobile-v1";
+const ASSETS=["./","./index.html","./manifest.webmanifest","./icons/icon-180.png","./icons/icon-512.png"];
+self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
+self.addEventListener("activate",e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));
+self.addEventListener("fetch",e=>{
+  if(e.request.method!=="GET") return;
+  e.respondWith(caches.match(e.request).then(cached=>cached||fetch(e.request).then(r=>{
+    const copy=r.clone(); caches.open(CACHE).then(c=>c.put(e.request,copy)); return r;
+  }).catch(()=>cached)));
+});
